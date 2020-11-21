0b547cf97dfc0cf8414a1b7c8e406a7f28550386
akryilov <ka_dikk@mail.ru>
2020-11-21 21:21:59 +0300

    add homework for lesson 6

---
+++ Lesson6_task1.py
@@ -0,0 +1,34 @@
+# 1. Создать класс TrafficLight (светофор) и определить у него один атрибут color (цвет) и метод running (запуск).
+# Атрибут реализовать как приватный.
+# В рамках метода реализовать переключение светофора в режимы: красный, желтый, зеленый.
+# Продолжительность первого состояния (красный) составляет 7 секунд, второго (желтый) — 2 секунды, третьего (зеленый)
+# — на ваше усмотрение.
+# Переключение между режимами должно осуществляться только в указанном порядке (красный, желтый, зеленый).
+# Проверить работу примера, создав экземпляр и вызвав описанный метод.
+
+
+
+from itertools import cycle  # import cycle function from "itertools" module
+import time  # import "time" module
+
+
+class TrafficLight:  # creating TrafficLight class
+    def __init__(self):  # initialization
+        self._color = ['Red', 'Yellow', 'Green']  # 'colors' attribute
+
+    def running(self):  # creating 'running' method
+        for el in cycle(self._color):
+            if el == 'Red':
+                print(el)
+                time.sleep(7)
+            if el == 'Yellow':
+                print(el)
+                time.sleep(2)
+            if el == 'Green':
+                print(el)
+                time.sleep(10)
+
+
+traffic_light_1 = TrafficLight()  #
+traffic_light_1.running()
+
---
+++ Lesson6_task3.py
@@ -0,0 +1,59 @@
+# 3. Реализовать базовый класс Worker (работник), в котором определить атрибуты:
+# name, surname, position (должность), income (доход).
+# Последний атрибут должен быть защищенным и ссылаться на словарь, содержащий элементы:
+# оклад и премия, например, {"wage": wage, "bonus": bonus}. Создать класс Position (должность) на базе класса Worker.
+#
+# В классе Position реализовать методы получения полного имени сотрудника (get_full_name)
+# и дохода с учетом премии (get_total_income).
+#
+# Проверить работу примера на реальных данных
+# (создать экземпляры класса Position, передать данные, проверить значения атрибутов, вызвать методы экземпляров).
+
+# class Worker:
+#     def __init__(self, position):
+#         self.name = 'Ivan'
+#         self.surname = 'Petrov'
+#         self.position = 'CEO'
+#         self._income = {"wage": 10, "bonus": 30}
+#
+# class Position(Worker):
+#
+#     def get_full_name(self):
+#         if self.position == 'CEO':
+#             print(f'Full Name: {self.name + " " + self.surname}')
+#         else:
+#             print('Person not found!')
+#
+#     def get_total_income(self):
+#         if self.position == 'CEO':
+#             print(f'Total income: {sum(self._income.values())}')
+#         else:
+#             print('Person not found!')
+#
+# ex_1 = Position('CEO')
+# ex_1.get_full_name()
+# ex_1.get_total_income()
+
+class Worker:
+    def __init__(self, name, surname, position, income):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {'wage': income*0.7, 'bonus': income*0.3}  # set dictionary where wage = 70%, bonus = 30%
+
+class Position(Worker):
+
+    def get_full_name(self):
+        print(f'Full Name: {self.name + " " + self.surname}')
+
+    def get_total_income(self):
+        print(f'Total income: {sum(self._income.values())}')
+
+    def get_bonus(self):
+            print(f'Bonus: {self._income.setdefault("bonus")}')
+
+
+ex_1 = Position('Ivan','Petrov','CEO', 20000)
+ex_1.get_full_name()
+ex_1.get_bonus()
+ex_1.get_total_income()
